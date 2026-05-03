9f1a870923b1974e53e89c5ddefeb6f70479469b
Rachel Evans <rachel@rachelevans.org>
2026-05-03 21:51:33 +0200

    feat: arrange content details in accordions

--- dropbox-hacking-photo-manager-next/src/app/(pages)/next-gen/content-hash/[contentHash]/ShowContentHashResult.tsx
+++ dropbox-hacking-photo-manager-next/src/app/(pages)/next-gen/content-hash/[contentHash]/ShowContentHashResult.tsx
@@ -4,6 +4,11 @@ import GeoMap from "@components/map/GeoMap";
 import SamePageLink from "@components/samePageLink";
 import ShowData from "@components/ShowData";
 import { useLatestValueFromServerFeed } from "@hooks/useLatestValueFromServerFeed";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Stack from "@mui/material/Stack";
 import {
   GPSLatLong,
   type GPSLatNLongE,
@@ -46,46 +51,39 @@ export const ShowContentHashResult = ({
 
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div style={{ marginInline: "1em" }}>
+      <Stack direction={"row"} sx={{ columnGap: "3em" }}>
+        <Stack direction={"column"} sx={{ rowGap: "3em" }}>
           <ImagePreview
             namedFile={latestValue.namedFiles[0]}
             photo={latestValue.photo ?? {}}
           />
 
           {latestValue.gps.effective && L && (
-            <div style={{ marginBlock: "1em" }}>
-              <GeoMap
-                positions={
-                  new Map([
-                    [
-                      contentHash,
-                      {
-                        position: new L.LatLng(
-                          latestValue.gps.effective.lat,
-                          latestValue.gps.effective.long,
-                        ),
-                        highlighted: false,
-                      },
-                    ],
-                  ])
-                }
-              />
-            </div>
+            <GeoMap
+              positions={
+                new Map([
+                  [
+                    contentHash,
+                    {
+                      position: new L.LatLng(
+                        latestValue.gps.effective.lat,
+                        latestValue.gps.effective.long,
+                      ),
+                      highlighted: false,
+                    },
+                  ],
+                ])
+              }
+            />
           )}
 
           <ShowData data={latestValue} />
-        </div>
+        </Stack>
 
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          {latestValue.exif && <SummariseExif exif={latestValue.exif} />}
-          {latestValue.mediaInfo && (
-            <SummariseMediaInfo mediaInfo={latestValue.mediaInfo} />
-          )}
-
-          <div style={{ marginBlock: "1em" }}>
-            {dayData ? (
-              dayData.files.length === 1 ? (
+        <Stack direction={"column"}>
+          {dayData ? (
+            <div style={{ marginBlock: "1em" }}>
+              {dayData.files.length === 1 ? (
                 <p>
                   The only file from{" "}
                   <SamePageLink
@@ -118,52 +116,94 @@ export const ShowContentHashResult = ({
                     {latestValue.date}
                   </SamePageLink>
                 </p>
-              )
-            ) : (
-              "loading..."
-            )}
-            {/* TODO, indicate >1 day */}
-            {/* TODO, make editable */}
-            <p style={{ marginBlock: "1em" }}>
-              Day ({latestValue.date}) description:{" "}
-              {dayData ? dayData.dayMetadata?.description || "-" : "loading..."}
-            </p>
-            <EditablePhotoEntry
-              contentHash={contentHash}
-              photoDbEntry={latestValue.photo ?? {}}
-            />
-            <EditableGPS
-              contentHash={contentHash}
-              photoDbEntry={latestValue.photo ?? {}}
-            />
-          </div>
+              )}
+            </div>
+          ) : (
+            "loading..."
+          )}
 
-          <p>Rotation: {latestValue.photo?.rotate ?? 0}°</p>
+          <p style={{ marginBlock: "1em" }}>
+            Description:{" "}
+            {dayData ? dayData.dayMetadata?.description || "-" : "loading..."}
+          </p>
 
-          <div>
-            <h2>GPS</h2>
-            <ol>
-              <li>
-                Embedded:{" "}
-                {gpsOrNone(
-                  `${contentHash} from content`,
-                  latestValue.gps.fromContent,
-                )}
-              </li>
-              <li>
-                Override:{" "}
-                {gpsOrNone(
-                  `${contentHash} from override`,
-                  latestValue.gps.fromOverride,
-                )}{" "}
-                <button>edit</button>
-              </li>
-            </ol>
-          </div>
+          {/* TODO, indicate >1 day */}
+          {/* TODO, make editable */}
 
-          <SummariseNamedFiles namedFiles={latestValue.namedFiles} />
-        </div>
-      </div>
+          {latestValue.exif && (
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <h3>Exif</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <SummariseExif exif={latestValue.exif} />
+              </AccordionDetails>
+            </Accordion>
+          )}
+
+          {latestValue.mediaInfo && (
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <h3>MediaInfo</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <SummariseMediaInfo mediaInfo={latestValue.mediaInfo} />
+              </AccordionDetails>
+            </Accordion>
+          )}
+
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <h3>Files</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <SummariseNamedFiles namedFiles={latestValue.namedFiles} />
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <h3>Description & Tags</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <EditablePhotoEntry
+                contentHash={contentHash}
+                photoDbEntry={latestValue.photo ?? {}}
+              />
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <h3>GPS</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <ol>
+                <li>
+                  Embedded:{" "}
+                  {gpsOrNone(
+                    `${contentHash} from content`,
+                    latestValue.gps.fromContent,
+                  )}
+                </li>
+                <li>
+                  Override:{" "}
+                  {gpsOrNone(
+                    `${contentHash} from override`,
+                    latestValue.gps.fromOverride,
+                  )}{" "}
+                  <button>edit</button>
+                </li>
+              </ol>
+
+              <EditableGPS
+                contentHash={contentHash}
+                photoDbEntry={latestValue.photo ?? {}}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </Stack>
+      </Stack>
     </>
   );
 };
