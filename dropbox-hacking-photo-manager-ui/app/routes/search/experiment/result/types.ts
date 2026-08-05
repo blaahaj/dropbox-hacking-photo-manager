926ae38a8c87ec60ff0d20f89f78b81233faf844
Rachel Evans <rachel@rachelevans.org>
2026-08-05 23:06:23 +0200

    chore: relax Formatter.styles type

--- dropbox-hacking-photo-manager-ui/app/routes/search/experiment/result/types.ts
+++ dropbox-hacking-photo-manager-ui/app/routes/search/experiment/result/types.ts
@@ -2,9 +2,10 @@ import type { ContentHashCollectionWithDay } from "dropbox-hacking-photo-manager
 import type { ReactNode } from "react";
 
 export type Formatter = {
-  readonly styles: Readonly<
-    Record<"page" | "result" | "selected" | "focused", string | undefined>
-  >;
+  readonly styles: Readonly<Record<string, string>>;
+  // readonly styles: Readonly<
+  //   Record<"page" | "result" | "selected" | "focused", string>
+  // >;
   readonly Result: (props: {
     readonly c: ContentHashCollectionWithDay;
     readonly onSelected: (value: boolean) => void;
